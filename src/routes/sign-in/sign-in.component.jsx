af143f234e81cc8f7cc06a6f30cd401b24c33ee1
alhadad-xyz <alhadad.xyz@gmail.com>
2023-08-20 12:28:18 +0700

    98. Sign Up Form Pt.1

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,23 +1,10 @@
-import { useEffect } from "react";
-import { getRedirectResult } from "firebase/auth";
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 import {
-  auth,
   signInWithGooglePopup,
   createUserDocumentFromAuth,
-  signInWithGoogleRedirect,
 } from "../../utils/firebase/firebase.util";
 
 const SignIn = () => {
-  useEffect(() => {
-    const handleRedirectResult = async () => {
-      const response = await getRedirectResult(auth);
-      if (response) {
-        const userDocRef = await createUserDocumentFromAuth(response.user);
-      }
-    };
-    handleRedirectResult();
-  }, []);
-
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
@@ -27,9 +14,7 @@ const SignIn = () => {
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with google pop up</button>
-      <button onClick={signInWithGoogleRedirect}>
-        Sign in with google Redirect
-      </button>
+      <SignUpForm />
     </div>
   );
 };
